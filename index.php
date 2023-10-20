97bc54928ef7fe58ae05eadf5501e9303dfd1ea4
Anderson Teala <andersonteala@gmail.com>
2023-10-20 20:40:19 -0300

    I separated the driver path settings to generate the log, and some adjustments to the index.php to standardize and load the config

--- index.php
+++ index.php
@@ -2,21 +2,32 @@
 
 require 'vendor/autoload.php';
 
+$configLogging = require('src/Config/Logging.php');
+
 use Proxy\Proxy;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+
+header("Content-Type: 'json");
 
 if (isset($_GET['url'])) {
   $url = $_GET['url'];
 } else {
   http_response_code(400);
-  echo json_encode(['error' => '"url" parameter not provided']);
+  echo json_encode(['error' => 'url parameter not provided']);
+  return false;
+}
+
+if(empty($url)){
+  http_response_code(400);
+  echo json_encode(['error' => 'url parameter empty']);
+  return false;
 }
 
 $logArquivoCaminho = 'my-php-proxy-' . date('Y-m-d') . '.log';
 
 $log = new Logger('index.php');
-$log->pushHandler(new StreamHandler('storage/logs/' . $logArquivoCaminho, Logger::DEBUG));
+$log->pushHandler(new StreamHandler($configLogging['path_default'] . $logArquivoCaminho, Logger::DEBUG));
 
 $proxy = new Proxy($url);
 
