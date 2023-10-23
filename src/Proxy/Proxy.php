91963f35077a948f6d24020b72e924cbb42603d9
Anderson Teala <andersonteala@gmail.com>
2023-10-23 14:32:35 -0300

    Settings for the proxy using the POST method

--- src/Proxy/Proxy.php
+++ src/Proxy/Proxy.php
@@ -26,6 +26,22 @@ class Proxy implements ProxyLoggerInterface
 
     $ch = curl_init($this->url);
 
+    $this->logger->info('REQUEST_METHOD', [$_SERVER['REQUEST_METHOD']]);
+
+    if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+      curl_setopt($ch, CURLOPT_POST, true);
+      $post_data = file_get_contents('php://input');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+      $request_headers = getallheaders();
+      $curl_headers = array();
+
+      foreach ($request_headers as $key => $value) {
+        $curl_headers[] = $key . ': ' . $value;
+      }
+
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
+    }
+
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_HEADER, false);
